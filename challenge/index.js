65a3e3168e53e5772b5ce320b85ca798f1fa2903
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-06 12:00:40 +0000

    First five tests completed

---
+++ challenge/index.js
@@ -0,0 +1,26 @@
+const fields = document.querySelectorAll("input", "textarea");
+
+fields.forEach((field) => {
+  field.setAttribute("aria-invalid", "false");
+
+  const feedback = document.createElement("p");
+  const id = field.id + "Error";
+  feedback.setAttribute("id", id);
+
+  const prevIds = field.getAttribute("aria-describedBy");
+  const describedBy = prevIds ? prevIds + " " + id : id;
+  field.setAttribute("aria-describedBy", describedBy);
+
+  field.after(feedback);
+
+  field.addEventListener("invalid", () => {
+    field.setAttribute("aria-invalid", "true");
+    const message = field.validationMessage;
+    feedback.textContent = message;
+  });
+
+  field.addEventListener("input", () => {
+    field.setAttribute("aria-invalid", "false");
+    feedback.textContent = "";
+  });
+});
